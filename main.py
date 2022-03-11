8bd4a4a59db3fc715395a21d57e885711105956d
Maxim <76528012+saQult@users.noreply.github.com>
2022-03-11 10:31:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -26,33 +26,35 @@ class MusicHandler(BaseHTTPRequestHandler):
     self.send_response_only(200)
     song_info = loads(self.rfile.read(int(self.headers['Content-Length'])))
     print(song_info)
-    
-    if song_info['is_paused']:
-      RPC.clear()
-      return  
+    try:
+      if song_info['is_paused']:
+        RPC.clear()
+        return  
 
-    name = song_info['name']
-    image = song_info['avatar']
-    artist = song_info['artist']
-    time_passed = int(song_info['time_passed'])
-    duration = int(song_info['duration'])
-    url = song_info['url']
-    buttons = [
-      {
-        'label': 'Listen SoundCloud',
-        'url': url
-      } 
-    ]
-    RPC.update(pid = getpid(),
-          details = name,
-          state = 'by ' + artist,
-          large_image = image,
-          small_image = 'https://m.sndcdn.com/_next/static/images/apple-touch-icon-180-893d0d532e8fbba714cceb8d9eae9567.png',
-          large_text = name, # не ебу зачем это надо
-          small_text =artist, # и это тоже
-          buttons=buttons,
-          end=time() + duration - time_passed
-          )
+      name = song_info['name']
+      image = song_info['avatar']
+      artist = song_info['artist']
+      time_passed = int(song_info['time_passed'])
+      duration = int(song_info['duration'])
+      url = song_info['url']
+      buttons = [
+        {
+          'label': 'Listen SoundCloud',
+          'url': url
+        } 
+      ]
+      RPC.update(pid = getpid(),
+            details = name,
+            state = 'by ' + artist,
+            large_image = image,
+            small_image = 'https://m.sndcdn.com/_next/static/images/apple-touch-icon-180-893d0d532e8fbba714cceb8d9eae9567.png',
+            large_text = name, # не ебу зачем это надо
+            small_text =artist, # и это тоже
+            buttons=buttons,
+            end=time() + duration - time_passed
+            )
+    except Exception:
+      pass
 
 if __name__ == "__main__":        
   webServer = HTTPServer((hostName, serverPort), MusicHandler)
