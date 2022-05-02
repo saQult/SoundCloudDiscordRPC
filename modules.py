4c3e297b0b0cf99846bdc8d026b1f8c62bd23e61
Maxim <76528012+saQult@users.noreply.github.com>
2022-05-02 22:55:30 +0500

    Update modules.py

--- modules.py
+++ modules.py
@@ -2,7 +2,6 @@ from psutil import process_iter, NoSuchProcess, AccessDenied, ZombieProcess
 
 
 def check_if_process_running(process_name):
-
     for proc in process_iter():
         try:
             if process_name.lower() in proc.name().lower():
